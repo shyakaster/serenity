707e9ca7a9fbc1ce18bb84dc89a904cffa8b7fad
alex shyaka <shyakaster@gmail.com>
2016-11-22 13:14:47 +0300

    refactor for rails

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'contacts/new', to: 'contacts#new', as: 'contactus'
 
   get 'contacts/create'
-
+  resources conatcts
   get 'gallery/index', to: 'gallery#index', as: 'gallery'
 
   get 'events/index', to: 'events#index', as: 'events'
