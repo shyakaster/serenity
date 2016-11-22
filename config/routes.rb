c7a0a366771914eab11296382f329512552165b9
alex shyaka <shyakaster@gmail.com>
2016-11-22 13:20:07 +0300

    change route format for contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'contacts/new', to: 'contacts#new', as: 'contactus'
 
-  get 'contacts/create'
-  resources conatcts
+  resources :contacts, only:[:new,:create]
+
   get 'gallery/index', to: 'gallery#index', as: 'gallery'
 
   get 'events/index', to: 'events#index', as: 'events'
